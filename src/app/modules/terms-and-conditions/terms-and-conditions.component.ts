331cf3c42d12eb50e91c0ffe50ce998710370dc2
Umesh Thapa <umeshthapa112233@gmail.com>
2023-02-14 15:44:38 +0545

    fix: added new pages

---
+++ src/app/modules/terms-and-conditions/terms-and-conditions.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: './terms-and-conditions.component.html',
+    styleUrls: ['./terms-and-conditions.scss']
+})
+export class TermsAndConditionsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
